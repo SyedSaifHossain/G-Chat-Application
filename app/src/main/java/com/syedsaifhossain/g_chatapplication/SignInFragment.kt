750f210b854d145457ed4bcc5582abc72d93c16b
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-03-20 06:45:56 +0600

    Login button added on signin_next_Fragment xml layout

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/SignInFragment.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/SignInFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.syedsaifhossain.g_chatapplication.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
@@ -16,8 +17,9 @@ class SignInFragment : Fragment() {
 
         binding = FragmentSignInBinding.inflate(inflater, container, false)
 
-
+binding.signInButton.setOnClickListener{
+    findNavController().navigate(R.id.signInFragment_to_signInNextFragment)
+}
         return binding.root
     }
-
 }
