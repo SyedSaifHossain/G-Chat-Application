70cc28763190aa34625a1d3a637a4e42f6ca7830
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-04-21 12:28:07 +0600

    pull request for add send code accepted

---
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/models/User.kt
@@ -0,0 +1,8 @@
+package com.syedsaifhossain.g_chatapplication.models
+
+data class User(
+    val uid: String = "",
+    val name: String = "",
+    val profileImageUrl: String = "",
+    val lastMessage: String = ""
+)
