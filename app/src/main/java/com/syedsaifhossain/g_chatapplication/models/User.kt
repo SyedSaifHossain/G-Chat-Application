26f4aa689d34d6c7b6c763ee0cc20897cf51f59c
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-04-22 09:55:34 +0600

    Chataddbutton now clickable for selecting album camera and video call

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/models/User.kt
+++
@@ -1,8 +0,0 @@
-package com.syedsaifhossain.g_chatapplication.models
-
-data class User(
-    val uid: String = "",
-    val name: String = "",
-    val profileImageUrl: String = "",
-    val lastMessage: String = ""
-)
