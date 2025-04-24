789965437efed98b2add6fb217bbd6d3179a7cc4
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-04-25 00:11:41 +0600

    Video call initialize all id

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/VideoCallFragment.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/VideoCallFragment.kt
@@ -1,59 +1,58 @@
 package com.syedsaifhossain.g_chatapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.syedsaifhossain.g_chatapplication.databinding.FragmentVideoCallBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [VideoCallFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class VideoCallFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentVideoCallBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video_call, container, false)
+    ): View {
+        _binding = FragmentVideoCallBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment VideoCallFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VideoCallFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.userName.text = "User Name"
+        binding.userStatus.text = "View Her Recent Activity"
+
+        binding.micButton.setOnClickListener {
+            // Toggle mic
+        }
+
+        binding.speakerButton.setOnClickListener {
+            // Toggle speaker
+        }
+
+        binding.cameraButton.setOnClickListener {
+            // Toggle camera
+        }
+
+        binding.endCallButton.setOnClickListener {
+
+        }
+
+        binding.switchCameraButton.setOnClickListener {
+            // Switch camera
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
