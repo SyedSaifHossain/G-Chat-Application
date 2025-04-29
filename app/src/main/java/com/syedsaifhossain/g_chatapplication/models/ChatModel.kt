85902d4ad97577b64c25dcf17a678af47df75369
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-04-29 13:09:36 +0600

    Change some code for fix bug issue for chat page and designed some layout

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/models/ChatModel.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/models/ChatModel.kt
@@ -3,6 +3,6 @@ package com.syedsaifhossain.g_chatapplication.models
 data class ChatModel(
     val senderId: String = "",
     val message: String = "",
-    val imageUrl: String? = null, // for stickers/images
-    val timestamp: Long = 0L
+    val timestamp: Long? = null,
+    val imageUrl: String? = null
 )
