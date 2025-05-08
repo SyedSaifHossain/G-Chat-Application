b3ac7bf7d1ce54bad61495d9d3956a1061ab81ac
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-05-08 22:51:10 +0600

    Video call update

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/MainActivity.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 // enableEdgeToEdge might need careful handling with locale changes,
 // but we'll keep it for now as per "no UI change" instruction.
 import androidx.activity.enableEdgeToEdge
-// No longer directly inheriting from AppCompatActivity
-// import androidx.appcompat.app.AppCompatActivity // This line is no longer needed
 
-// Inherit from BaseActivity instead of AppCompatActivity
 class MainActivity : BaseActivity() { // <--- This is the change
 
     override fun onCreate(savedInstanceState: Bundle?) {
