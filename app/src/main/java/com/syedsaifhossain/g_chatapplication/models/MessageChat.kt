bec0dbac400873a5afbb839b119c6b38144488af
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-05-14 06:46:00 +0600

    change some code

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/models/MessageChat.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/models/MessageChat.kt
@@ -1,7 +1,7 @@
 package com.syedsaifhossain.g_chatapplication.models
 
 data class MessageChat(
-
-    var messageChat : String? = null,
-    var senderId : String? = null
+    var messageChat: String? = null,
+    var senderId: String? = null,
+    var timestamp: Long? = null
 )
