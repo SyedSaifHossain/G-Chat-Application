99c62222753f88ae3c0d634784d3170e2381d314
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-05-15 08:01:43 +0600

    User model added

---
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/models/User.kt
@@ -0,0 +1,9 @@
+package com.syedsaifhossain.g_chatapplication.models
+
+data class User(
+    val name: String? = null,
+    val phone: String? = null,
+    val uid: String? = null,
+    val profilePicUrl: String? = null,
+    val lastMessage: String? = null
+)
