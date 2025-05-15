af4de8dafc44ebb52eb36e4e361cc84c7616bed2
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-05-15 07:43:36 +0600

    User adapter code update

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/adapter/UserAdapter.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/adapter/UserAdapter.kt
@@ -1,4 +1,35 @@
 package com.syedsaifhossain.g_chatapplication.adapter
 
-class UserAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.auth.User
+import com.syedsaifhossain.g_chatapplication.databinding.UserLayoutBinding
+import com.syedsaifhossain.g_chatapplication.models.User
+
+class UserAdapter(
+    private val userList: ArrayList<User>,
+    private val onUserClicked: (User) -> Unit
+) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    inner class UserViewHolder(private val binding: UserLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User){
+            binding.userName.text = user.phone?:"Unknown"
+            binding.root.setOnClickListener {
+                onUserClicked(user)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = UserLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(userList[position])
+    }
+
+    override fun getItemCount(): Int = userList.size
 }
