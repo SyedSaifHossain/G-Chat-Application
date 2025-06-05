0c37216ac602f6e7e04a990d1349ff1f99f52e17
Ning <ningcc18@gmail.com>
2025-06-05 13:21:43 +1000

    features：Group Chat

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/models/Chats.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/models/Chats.kt
@@ -9,6 +9,7 @@ data class Chats(
     @DrawableRes val imageRes: Int = 0,      // Optional, for local avatar resource
     val name: String = "",                   // Other user's name
     val message: String = "",                // Last message preview
+    val type: String = "text",               // 新增，消息类型：text/voice
     val otherUserId: String = "",            // ID of the other user
     val otherUserAvatarUrl: String? = null,  // URL string for the other user's avatar
     val isGroup: Boolean = false,             // 新增
