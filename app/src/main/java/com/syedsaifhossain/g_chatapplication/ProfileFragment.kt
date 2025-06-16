e8e36020b52f7c277ea33f28d786392202922f9c
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-06-16 20:34:35 +0600

    Profile fragment update

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/ProfileFragment.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/ProfileFragment.kt
@@ -1,24 +1,11 @@
 package com.syedsaifhossain.g_chatapplication
 
-import android.app.Activity
-import android.app.AlertDialog
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.text.InputType
 import android.view.*
-import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
 import com.syedsaifhossain.g_chatapplication.databinding.FragmentProfileBinding
-import java.util.*
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -29,7 +16,7 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
@@ -37,8 +24,12 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-binding.profileBackImg.setOnClickListener {  }
-        findNavController().popBackStack()
+
+        binding.profileBackImg.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+
     }
 
 
