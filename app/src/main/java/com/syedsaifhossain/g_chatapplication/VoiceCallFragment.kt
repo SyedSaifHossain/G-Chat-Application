4a94c6020ec3ffef49afd5a66a4db9021ce39bd4
Ning <ningcc18@gmail.com>
2025-06-18 17:27:25 +1000

    features：can choose the country in profile page

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/VoiceCallFragment.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/VoiceCallFragment.kt
@@ -200,9 +200,9 @@ class VoiceCallFragment : Fragment() {
         Log.d("VoiceCall", "Agora UID: $agoraUid")
 
         val client = OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .build()
             
         val request = Request.Builder().url(url).build()
