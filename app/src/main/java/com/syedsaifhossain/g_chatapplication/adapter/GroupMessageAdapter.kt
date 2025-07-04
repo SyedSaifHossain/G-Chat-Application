08d28b540dc1819a17a0127d5d649d91921e32b7
Syed Saif Hossain <syedsaifnibir@gmail.com>
2025-07-04 12:11:12 +0600

    Image and video ui modify

--- app/src/main/java/com/syedsaifhossain/g_chatapplication/adapter/GroupMessageAdapter.kt
+++ app/src/main/java/com/syedsaifhossain/g_chatapplication/adapter/GroupMessageAdapter.kt
@@ -70,7 +70,8 @@ class GroupMessageAdapter(
                     binding.sentMessageVideo.visibility = View.GONE
                     binding.sentMessageVoice.visibility = View.GONE
                     binding.sentMessageFile.visibility = View.GONE
-                    
+                    binding.qroupChatSendTextBox.visibility = View.GONE
+
                     Glide.with(binding.root.context)
                         .load(message.imageUrl)
                         .placeholder(R.drawable.default_image)
@@ -83,6 +84,7 @@ class GroupMessageAdapter(
                     binding.sentMessageImage.visibility = View.GONE
                     binding.sentMessageVoice.visibility = View.GONE
                     binding.sentMessageFile.visibility = View.GONE
+                    binding.qroupChatSendTextBox.visibility = View.GONE
 
                     Glide.with(binding.root.context)
                         .load(message.videoUrl)
